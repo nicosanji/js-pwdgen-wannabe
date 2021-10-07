dda269f685fd6a8a92f65f4e86998488e0e2f1ef
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-07 14:45:54 +0200

    START: creati n.3 prompt e n.1 alert

---
+++ myscript.js
@@ -0,0 +1,13 @@
+/* Chiedere all'utente nome, cognome, colore preferito */
+
+const nomeUtente = prompt("Inserisci il tuo nome.");
+const cognomeUtente = prompt("Inserisci il tuo cognome.");
+const coloreUtente = prompt("Inserisci il tuo colore preferito.");
+
+console.log("nome / cognome / colore:", nomeUtente, cognomeUtente, coloreUtente)
+
+/* Mostra all'utente nomecognomecolore21 */
+
+alert(nomeUtente + cognomeUtente + coloreUtente + "21");
+
+console.log("risultato alert:", nomeUtente + cognomeUtente + coloreUtente + "21");
